609ea218ef8d8e2c55d57a713343fbda200279c5
GeraDV <gerardodamian.velazquez@estudiantes.unahur.edu.ar>
2024-12-06 01:46:01 -0300

    Se refactorizaron y unificaron los esquemas de joi para hacerlos más reutilizables... Se agregaron validaciones de Permiso para que un usuario pueda solamente modificar sus datos y no los de otro usuario... Se agregó una ruta PATCH para actualizar el nombre o la contraseña

--- src/schemas/usuarios.schema.js
+++ src/schemas/usuarios.schema.js
@@ -1,22 +1,45 @@
 const Joi = require('joi')
 
-const usuarioSchema = Joi.object({
-  nombre: Joi.string().required().min(3).max(255).messages({
-    "string.base":"El nombre debe ser una cadena de texto",
-    "string.min":"El nombre debe tener al menos {#limit} caracteres",
-    "string.max":"El nombre no debe tener más de {#limit} caracteres",
-    "any.required":"El nombre es obligatorio",
-  }),
-  email: Joi.string().email().required().messages({
-    "string.base":"El correo debe ser una cadena de texto",
-    "string.email":"El correo debe tener un formato válido",
-    "any.required":"El correo es obligatorio",
-  }),
-  password: Joi.string().required().min(6).max(255).messages({
-    "string.base":"La contraseña debe ser una cadena de texto",
-    "string.min":"La contraseña debe tener al menos {#limit} caracteres",
-    "any.required":"La contraseña es obligatoria",
-  })
+const nombre = Joi.string().min(3).max(255).messages({
+  "string.base":"El nombre debe ser una cadena de texto",
+  "string.min":"El nombre debe tener al menos {#limit} caracteres",
+  "string.max":"El nombre no debe tener más de {#limit} caracteres",
+  "any.required":"El nombre es obligatorio",
 })
 
-module.exports = usuarioSchema
+const nombreObligatorio = nombre.required().messages({
+  "any.required":"El nombre es obligatorio"
+})
+
+const emailObligatorio = Joi.string().email().required().messages({
+  "string.base":"El correo debe ser una cadena de texto",
+  "string.email":"El correo debe tener un formato válido",
+  "any.required":"El correo es obligatorio",
+})
+
+const password = Joi.string().min(6).max(255).messages({
+  "string.base":"La contraseña debe ser una cadena de texto",
+  "string.min":"La contraseña debe tener al menos {#limit} caracteres",  
+})
+
+const passwordObligatorio = password.required().messages({
+  "any.required":"La contraseña es obligatoria"
+})
+
+const usuarioSchema = Joi.object({
+  nombre: nombreObligatorio, 
+  email: emailObligatorio, 
+  password: passwordObligatorio
+})
+
+const loginSchema = Joi.object({
+  email: emailObligatorio, 
+  password: passwordObligatorio
+})
+
+const actualizarUsuarioSchema = Joi.object({
+  nombre,
+  password
+}).or('nombre', 'password')
+
+module.exports = {usuarioSchema, loginSchema, actualizarUsuarioSchema}
